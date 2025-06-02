254c702b621bec90f838438fcd7d757cebed4ed1
ToshihitoKomatsu <komatsu@dsnf.co.jp>
2025-06-02 10:58:46 +0900

    feat: ボタンにuseNavigateフックを使用してページ遷移を実装

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,12 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export const Page1Layout = () => {
+  const navigate = useNavigate();
+
+  const onClickLink = () => {
+    navigate('/page1/detailA');
+  }
+
   return (
     <div>
       <h1>Page1</h1>
       <Link to='/page1/detailA'>CssModules</Link>
       <br />
+      <br />
       <Link to='/page1/detailB'>DetailB</Link>
+      <br />
+      <br />
+      <button onClick={onClickLink}>CssModules</button>
     </div>
   )
 }
--- src/pages/Page1DetailA.tsx
+++ src/pages/Page1DetailA.tsx
@@ -3,8 +3,15 @@ import { CssModules } from '../components/CssModules'
 import { StyledJsx } from '../components/StyledJsx'
 import { StyledComponents } from '../components/StyledComponents'
 import { Emotion } from '../components/Emotion'
+import { useNavigate } from 'react-router-dom'
 
 export const Page1DetailA = () => {
+  const navigate = useNavigate();
+
+  const onClickBack = () => {
+    navigate(-1);
+  }
+
   return (
     <div>
       <h1>CssModules</h1>
@@ -15,6 +22,8 @@ export const Page1DetailA = () => {
         <StyledComponents />
         <Emotion />
       </div>
+      <br />
+      <button onClick={onClickBack}>戻る</button>
     </div>
   )
 }
