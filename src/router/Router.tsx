d23d965ebbff370355b0099712fbf1158338191c
ToshihitoKomatsu <komatsu@dsnf.co.jp>
2025-06-02 12:10:54 +0900

    feat: 404ページ（NotFoundページ）を実装

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,6 +4,7 @@ import { Page1Layout } from '../pages/Page1'
 import { Page1DetailA } from '../pages/Page1DetailA'
 import { Page1DetailB } from '../pages/Page1DetailB'
 import { Page2 } from '../pages/Page2'
+import { Page404 } from '../pages/Page404'
 
 export const Router = () => {
   return (
@@ -13,6 +14,7 @@ export const Router = () => {
       <Route path='/page1/detailA' element={<Page1DetailA />} />
       <Route path='/page1/detailB' element={<Page1DetailB />} />
       <Route path='/page2' element={<Page2 />} />
+      <Route path='*' element={<Page404 />} />
     </Routes>
   )
 } 
