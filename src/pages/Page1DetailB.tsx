e85345f43151f65a284c16940cd27cc8cb6d3279
ToshihitoKomatsu <komatsu@dsnf.co.jp>
2025-06-02 11:50:51 +0900

    feat: Page1DetailBに戻るボタンを実装

--- src/pages/Page1DetailB.tsx
+++ src/pages/Page1DetailB.tsx
@@ -1,7 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+
 export const Page1DetailB = () => {
+  const navigate = useNavigate();
+
+  const onClickBack = () => {
+    navigate(-1);
+  }
+
   return (
     <div>
       <h1>Page1DetailB</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   )
 }
